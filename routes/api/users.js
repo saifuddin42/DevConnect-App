9d9d9bea643e090d7e64b5f5774efcbf35f95a07
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-24 13:04:51 -0400

    added user detail validation using express validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+const { check, validationResult } = require('express-validator/check');
+
 /**
  * @route   POST api/users
  * @desc    Register User
  * @access  Public
  */
-router.post('/', (req, res) => {
-  console.log('User POST req sent: ', req.body);
-  res.send('Test User Route');
-});
+router.post(
+  '/',
+  // Added validation checks for user
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please enter a valid email address').isEmail(),
+    check(
+      'password',
+      'Please enter a valid password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    console.log('User POST req sent: ', req.body);
+    res.send('Test User Route');
+  }
+);
 
 module.exports = router;
