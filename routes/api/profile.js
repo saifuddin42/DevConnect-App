73b6788beb638a65013dec89de52123506ef27d4
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-24 16:24:31 -0400

    updated profile api to get profile of an individual user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 /**
- * @route   GET api/profile
- * @desc    Test rout
- * @access  Public
+ * @route   GET api/profile/me
+ * @desc    Get curent user's profile
+ * @access  Private
  */
-router.get('/', (re, res) => {
-  res.send('Test Profile Route');
+router.get('/me', auth, async (req, res) => {
+  // the auth authorizes if there is a token in the header of the request or not
+  try {
+    // Find the individual user profile
+    const profile = await Profile.findOne({ user: req.user.id }) // the user in here is the user field in the Profile schema!
+      .populate('user', ['name', 'avatar']); // populate the profile user field with name and avatar from user using their id
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json({ msg: 'Profile found', profile: profile });
+  } catch (err) {
+    console.error('Error loading profile: ', err.message);
+    res.status(500).send('Profile Server error!');
+  }
 });
 
 module.exports = router;
