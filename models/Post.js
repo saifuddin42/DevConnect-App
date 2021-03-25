cbd0e7bab0b6004f5904cdd234e7444a4d933190
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-24 21:53:40 -0400

    created Post schema

---
+++ models/Post.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  // the name here is the name of the user. Because if the user deletes their account I want to be able to not delete their post if we just want to keep the post
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
