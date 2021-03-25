437c3930d3cd27d7c330303a718e461c5e9494f9
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-25 16:07:48 -0400

    created alertReducer action

---
+++ client/src/actions/alert.js
@@ -0,0 +1,20 @@
+/**
+ * Actions for alertReducer.
+ */
+import { SET_ALERT, REMOVE_ALERT } from './types';
+import { v4 as uuidv4 } from 'uuid'; // to generate random id value for alert object
+
+const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
+  //dispatch is from thunk middleware in store.js
+  const id = uuidv4();
+
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id },
+  });
+
+  // remove alert after displaying for 5 seconds
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+};
+
+export default setAlert;
