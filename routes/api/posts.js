49f8aa01576d747f4b655bc06c4241ef6feccbd5
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-24 22:59:34 -0400

    created api endpoint for post likes and unlikes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -129,7 +129,7 @@ router.put('/like/:id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
-    // Check if the post has already been liked
+    // Check if the post has already been liked by the current user
     if (
       post.likes.filter((like) => like.user.toString() === req.user.id).length >
       0
@@ -137,6 +137,7 @@ router.put('/like/:id', auth, async (req, res) => {
       return res.status(400).json({ msg: 'Post already liked' });
     }
 
+    // push like to the start of array
     post.likes.unshift({ user: req.user.id });
 
     await post.save();
@@ -157,7 +158,7 @@ router.put('/unlike/:id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
-    // Check if the post has already been liked
+    // Check if the post has no likes by the current user
     if (
       post.likes.filter((like) => like.user.toString() === req.user.id)
         .length === 0
@@ -175,78 +176,6 @@ router.put('/unlike/:id', auth, async (req, res) => {
     await post.save();
 
     res.json({ msg: 'Post Unliked', post: post.likes });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send(' Posts Server Error');
-  }
-});
-
-/**
- * @route    POST api/posts/comment/:id
- * @desc     Comment on a post
- * @access   Private
- */
-router.post(
-  '/comment/:id',
-  [auth, [check('text', 'Text is required').not().isEmpty()]],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    try {
-      const user = await User.findById(req.user.id).select('-password');
-      const post = await Post.findById(req.params.id);
-
-      const newComment = {
-        text: req.body.text,
-        name: user.name,
-        avatar: user.avatar,
-        user: req.user.id,
-      };
-
-      post.comments.unshift(newComment);
-
-      await post.save();
-
-      res.json({ msg: 'Comment Posted', post: post.comments });
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send(' Posts Server Error');
-    }
-  }
-);
-
-/**
- * @route    DELETE api/posts/comment/:id/:comment_id
- * @desc     Delete comment
- * @access   Private
- */
-router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-
-    // Pull out comment
-    const comment = post.comments.find(
-      (comment) => comment.id === req.params.comment_id
-    );
-    // Make sure comment exists
-    if (!comment) {
-      return res.status(404).json({ msg: 'Comment does not exist' });
-    }
-    // Check user
-    if (comment.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'User not authorized' });
-    }
-
-    post.comments = post.comments.filter(
-      ({ id }) => id !== req.params.comment_id
-    );
-
-    await post.save();
-
-    res.json({ msg: 'Comment Deleted', post: post.comments });
   } catch (err) {
     console.error(err.message);
     res.status(500).send(' Posts Server Error');
