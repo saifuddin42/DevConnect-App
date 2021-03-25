fb08df5ff4b7373a162b7569b554cfe776261d94
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-25 12:05:46 -0400

    created basic components and setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => {
   return (
@@ -11,6 +13,12 @@ const App = () => {
       <Fragment>
         <Navbar />
         <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
       </Fragment>
     </Router>
   );
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Landing = () => {
   return (
     <section className="landing">
@@ -11,12 +13,12 @@ const Landing = () => {
             from other developers
           </p>
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">
+            <Link to="/register" className="btn btn-primary">
               Sign Up
-            </a>
-            <a href="login.html" className="btn btn-light">
+            </Link>
+            <Link to="/login" className="btn btn-light">
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
