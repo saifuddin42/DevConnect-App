b98fc20784ef405ce2a8d54c5400c795db1265dd
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-26 13:54:12 -0400

    register success and fail reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,19 +1,19 @@
 import {
   REGISTER_SUCCESS,
-  //REGISTER_FAIL,
+  REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  //LOGIN_FAIL,
+  LOGIN_FAIL,
   LOGOUT,
   ACCOUNT_DELETED,
 } from '../actions/types';
 
 const initialState = {
-  token: localStorage.getItem('token'),
-  isAuthenticated: null,
-  loading: true,
-  user: null,
+  token: localStorage.getItem('token'), // get the x-auth-token named 'token' stored in the local storage
+  isAuthenticated: null, // set to true after request to api returns proper token
+  loading: true, // set to false after making a request to the backend and getting a response
+  user: null, // set to user data object after getting the user from the api
 };
 
 function authReducer(state = initialState, action) {
@@ -29,10 +29,21 @@ function authReducer(state = initialState, action) {
       };
     case REGISTER_SUCCESS:
     case LOGIN_SUCCESS:
+      // save the token in storage
+      localStorage.setItem('token', payload.token);
       return {
         ...state,
         ...payload,
         isAuthenticated: true,
+        loading: false,
+      };
+    case REGISTER_FAIL:
+      // remove the token from storage
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
         loading: false,
       };
     case ACCOUNT_DELETED:
