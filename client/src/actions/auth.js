2eb494f28e4b852e2f30d8edc9de68ac7b658f37
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-26 13:54:36 -0400

    registration success and fail action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,6 +9,7 @@ import {
   LOGIN_FAIL,
   LOGOUT,
 } from './types';
+import axios from 'axios';
 
 // Load User
 export const loadUser = () => async (dispatch) => {
@@ -27,22 +28,35 @@ export const loadUser = () => async (dispatch) => {
 };
 
 // Register User
-export const register = (formData) => async (dispatch) => {
+export const register = ({ name, email, password }) => async (dispatch) => {
   try {
-    const res = await api.post('/users', formData);
+    // create header for request
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
 
+    // create body for request
+    const body = JSON.stringify({ name, email, password });
+
+    //send a request to api and await a promise from axios
+    const res = await axios.post('/api/users', body, config); // didn't need to use full link because package,json has a proxy set up with http://localhost:5000
+
+    // on success use dispatch
     dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data,
     });
     dispatch(loadUser());
   } catch (err) {
+    // get error message array
     const errors = err.response.data.errors;
-
+    // if they exist, alert them all out
     if (errors) {
       errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
     }
-
+    // then call register_fail
     dispatch({
       type: REGISTER_FAIL,
     });
