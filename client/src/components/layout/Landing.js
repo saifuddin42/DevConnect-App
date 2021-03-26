cf7d3b25b017b7a71f66bdc1e54e10cb5569c518
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-26 18:01:12 -0400

    added redirect for landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Landing = () => {
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
   return (
     <section className="landing">
       <div className="dark-overlay">
@@ -26,4 +32,16 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+// validating the prop types and enforcing rules
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+// Use when you want to fetch redux state into a component.
+const mapStateToProps = (state) => ({
+  // mapping the redux state to the props in this component
+  isAuthenticated: state.auth.isAuthenticated, // doing this will give me isAuthenticated from the auth reducer component { token: localStorage.getItem('token'), isAuthenticated: null, loading: true, user: null,}
+  profile: state.profile, // doing this will give me everything in the profile reducer component i.e. { profile: null, profiles: [], repos: [], loading: true, error: {}, // list of errors }
+});
+
+export default connect(mapStateToProps)(Landing);
