e893424168ba7204e72177b786c0434f74b21161
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-26 19:06:20 -0400

    added edit profile component

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,8 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
-import CreateProfile from './components/profile-forms/CreateProfile'; // for adding or editing profile
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -51,6 +52,11 @@ const App = () => {
                   exact
                   path="/create-profile"
                   component={CreateProfile}
+                />
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
                 />
               </section>
             </>
