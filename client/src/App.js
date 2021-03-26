c7048248855f1e272da3b4cd7b3498079b3b8caa
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-26 18:52:23 -0400

    added create profile component

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile'; // for adding or editing profile
 
 // Redux
 import { Provider } from 'react-redux';
@@ -46,6 +47,11 @@ const App = () => {
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </section>
             </>
           </Switch>
