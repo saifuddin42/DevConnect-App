708011b3b1034e25a9a109f8d88144a78fa7b4d7
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-27 11:50:08 -0400

    added comment

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -15,6 +15,7 @@ const PostItem = ({
 }) => (
   <div className="post bg-white p-1 my-1">
     <div>
+      {/* link to user profile */}
       <Link to={`/profile/${user}`}>
         <img className="round-img" src={avatar} alt="" />
         <h4>{name}</h4>
