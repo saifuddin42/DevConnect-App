5ac3046160be974398f65cdab487ed4c490cd850
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-27 10:22:14 -0400

    created user profile with complete details

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,103 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
+import { getProfileById } from '../../actions/profile';
+
+const Profile = ({
+  getProfileById,
+  profile: { profile, loading },
+  auth,
+  match,
+}) => {
+  useEffect(() => {
+    getProfileById(match.params.id);
+  }, [getProfileById, match.params.id]);
+
+  return (
+    <>
+      {/* if the profile has not loaded yet show spinner else show profile */}
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Link to="/profiles" className="btn btn-light">
+            Back To Profiles
+          </Link>
+          {/* if the profile being viewed is the same as the signed in user, show edit profile button  */}
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+          <div className="profile-grid my-1">
+            <ProfileTop profile={profile} />
+
+            <ProfileAbout profile={profile} />
+
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
+              {/* if experiences are available then map through them and show */}
+              {profile.experience.length > 0 ? (
+                <>
+                  {profile.experience.map((experience) => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </>
+              ) : (
+                // else print no experience
+                <h4>No experience credentials</h4>
+              )}
+            </div>
+
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+              {/* if education is available then map through it and show */}
+              {profile.education.length > 0 ? (
+                <>
+                  {profile.education.map((education) => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </>
+              ) : (
+                // else print no education
+                <h4>No education credentials</h4>
+              )}
+            </div>
+            {/* if github username exists then get last 5 repos */}
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired, // to get an edit button in the profile list if it's your own profile
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getProfileById })(Profile);
---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,37 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileAbout = ({
+  profile: {
+    bio,
+    skills,
+    user: { name },
+  },
+}) => (
+  <div className="profile-about bg-light p-2">
+    {/* show bio if it exists */}
+    {bio && (
+      <>
+        {/* name of the user */}
+        <h2 className="text-primary">{name.trim().split(' ')[0]}s Bio</h2>
+        <p>{bio}</p>
+        <div className="line" />
+      </>
+    )}
+    <h2 className="text-primary">Skill Set</h2>
+    {/* map through all skills and print */}
+    <div className="skills">
+      {skills.map((skill, index) => (
+        <div key={index} className="p-1">
+          <i className="fas fa-check" /> {skill}
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
+
+export default ProfileAbout;
