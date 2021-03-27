a5bf3cb4c085e18441c8e56e2e50b1a6824f64e5
Saifuddin Telia <saifuddintelia@gmail.com>
2021-03-27 11:13:55 -0400

    added posts component

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -9,8 +9,8 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
-export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 
+export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 export const GET_PROFILE = 'GET_PROFILE';
 export const GET_PROFILES = 'GET_PROFILES';
 export const GET_REPOS = 'GET_REPOS';
@@ -18,3 +18,6 @@ export const NO_REPOS = 'NO_REPOS';
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 export const PROFILE_ERROR = 'PROFILE_ERROR';
+
+export const GET_POSTS = 'GET_POSTS';
+export const POST_ERROR = 'POST_ERROR';
